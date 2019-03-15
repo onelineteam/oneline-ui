40dad076379a9878df312a73b49e08fc17f185eb
keep2zero <towaywu@gmail.com>
2019-03-15 11:41:44 +0800

    feat: 调整适合这个项目的相关配置

---
+++ .eslintrs.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": [
+    "tslint:recommended"
+],
+  globals: {
+    '__VUETIFY_VERSION__': true,
+    '__REQUIRED_VUE__': true
+  },
+  plugins: [
+    'eslint-plugin-local-rules'
+  ],
+  rules: {
+    'local-rules/no-render-string-reference': 'error'
+  }
+}
