f3da18e9f8e8f03c9e43f1a08524110b831b1f88
keep2zero <towaywu@gmail.com>
2019-03-15 11:43:12 +0800

    change: 动态引入hell.ts组件

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,10 +1,27 @@
 <template>
-  <h3>hello, world! test</h3>
+  <div>hello, world! test
+
+    <hello :outtitle="title"/>
+  </div>
 </template>
 
 <script>
+// import Hello from './hello';
+ 
 export default {
+  components: {Hello: ()=> import('./hello')},
   
+  data() {
+    return {
+      title: 'hello'
+    }
+  }, 
+  mounted() {
+    // console.log(Hello)
+    setTimeout(()=>{
+      this.title = "this is test"
+    },5000)
+  }
 }
 </script>
 
