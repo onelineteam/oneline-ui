e6447cf0e8b083d482f7d953fc0e19e9b01dbad2
keep2zero <towaywu@gmail.com>
2019-03-15 11:40:57 +0800

    change: 修改hello组件

---
+++ src/app/hello.ts
@@ -0,0 +1,43 @@
+/**
+ * @file  测试例子
+ * @author towaywu@gmail.com
+ * @date 2019-03-15
+ */
+import {CreateElement} from 'vue';
+import {Vue, Component, Model, Prop, Watch } from '@core';
+
+/**
+ * @class 用来测试title的prop和内部变量
+ */
+class TestTitle extends Vue {
+  @Prop() readonly outtitle?: string;
+  title: string = this.outtitle || "";
+}
+ 
+
+
+/**
+ * @class helloworld组件 演示了watch的用法
+ */
+@Component
+class  HelloWorld extends TestTitle {
+
+  @Watch('outtitle')
+  watchOuttitle(newval:string, oldval:string) {
+    console.log(newval)
+    this.title = newval;
+  }
+
+  mounted() {
+    setTimeout(()=>{
+       this.title = "test"
+    }, 3000)
+  }
+
+  render(h: CreateElement) {
+   
+    return h("h3", {}, "hello!" + this.title);
+  }
+}
+
+export default  HelloWorld
