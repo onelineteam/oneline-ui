5919b6b56a7cc3cc271870587810e4f9b5496bcd
keep2zero <towaywu@gmail.com>
2019-03-17 22:03:57 +0800

    chore: 清除icon中不用的代码

--- src/packages/icon/index.ts
+++
@@ -1,7 +0,0 @@
-import {OnelineComponent, CreateElement} from '@core';
-
-class Icon extends OnelineComponent {
-  render(h: CreateElement) {
-    return h('button', {class: this._class('button')})
-  }
-}
