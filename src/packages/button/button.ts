8b803b228e5a1588319ffaca4b805cf00d34a809
Calvin <767102105@qq.com>
2019-05-23 00:27:51 +0800

    fix:优化button组件

--- src/packages/button/button.ts
+++ src/packages/button/button.ts
@@ -1,13 +1,13 @@
 
 import { OnelineComponent, CreateElement, Component, Prop } from "@core";
-import {Icon} from '@/packages/icon';
- @Component({
+import { Icon } from '@/packages/icon';
+@Component({
   components: {
     Icon
-   }
- })
+  }
+})
 export class OneLineButton extends OnelineComponent {
-  @Prop({default:'default'}) type?: string;  //类型
+  @Prop({ default: 'default' }) type?: string;  //类型
   @Prop({ default: false, type: Boolean }) plain!: boolean; //是否朴素按钮
   @Prop({ default: false, type: Boolean }) round!: boolean; //是否圆角按钮
   @Prop({ default: false, type: Boolean }) circle!: boolean; //是否圆形按钮
@@ -15,25 +15,41 @@ export class OneLineButton extends OnelineComponent {
   @Prop({ default: false, type: Boolean }) loading!: boolean; //是否加载状态
   @Prop() size?: string;  //尺寸
   @Prop() icon?: string;  //类型
+
   render(create: CreateElement) {
-    
+    const className: any = [
+      "oneline-button",
+      "oneline-button--" + this.type,
+      this.size ? "oneline-button--" + this.size : "",
+      this.plain ? "is-plain" : "", this.round ? "is-round" : "",
+      this.circle ? "is-circle" : "", this.disabled ? "is-disabled" : "",
+      this.loading ? "is-loading" : ""
+    ]
+    let vnodes :any[]= [];
+    if (this.loading) {
+      vnodes = [create(Icon, {
+        props: {
+          type: "loading",
+          size: "16px",
+          color: "#FFFFFF"
+        }
+      }), create('span', this.$slots.default)]
+    } else if (this.icon) {
+      vnodes = [create(Icon, {
+        props: {
+          type: this.icon,
+          size: "16px",
+          color: "#FFFFFF"
+        }
+      }), create('span', this.$slots.default)]
+    } else {
+      vnodes = [create('span', this.$slots.default)]
+    }
     return create('button', {
-      class:["oneline-button","oneline-button--"+this.type,this.size?"oneline-button--"+this.size:"",this.plain?"is-plain":"",this.round?"is-round":"",this.circle?"is-circle":"",this.disabled?"is-disabled":"",this.loading?"is-loading":""],
-      on: {click: () => { this.$emit('click')}}
-    }, 
-    this.loading?[create(Icon,{
-        props:{
-          type:"loading",
-          size:"16px",
-          color:"#FFFFFF"
-        }
-      }),create('span',this.$slots.default)]:this.icon?[create(Icon,{
-        props:{
-          type:this.icon,
-          size:"16px",
-          color:"#FFFFFF"
-        }
-      }),create('span',this.$slots.default)]:[create('span',this.$slots.default)],
+      class: className,
+      on: { click: () => { this.$emit('click') } }
+    },
+      vnodes
     )
   };
 }
