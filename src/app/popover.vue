29c3b4c62a33032022884cb7e6e052a4208baf00
keep2zero <towaywu@gmail.com>
2019-09-16 23:07:57 +0800

    fix: 修复配置文件错误问题

---
+++ src/app/popover.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+  <popover trigger="focus" :content="title">
+    <!-- <button>{{title}}</button> -->
+    <textarea  />
+     
+  </popover>
+
+
+   <popover trigger="click">
+    <button>goggogogogog</button>
+    <div slot="title">hello</div>
+    <div slot="content" @click="hello">come on</div>
+    <div slot="content">引用的内容2=<br><br> <br>kadjsfljasdljfakldsjflkasdjf;lasd</div>
+  </popover>
+
+
+  <popover trigger="hands" :fixed="true" title="this is hand">
+    <button>我是另外一个按钮</button>
+ 
+    <div slot="content" @click="hello">引用的内容</div>
+    <div slot="content">引用的内容2fsdfsdfdsfdfakj<br>fla;dsjflakdjsflkjadslkfjlakdsjfl<br>kadjsfljasdljfakldsjflkasdjf;lasd</div>
+  </popover>
+
+
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+   return {
+     title: "我是按钮",
+     count: 0,
+     date: new Date()
+   }
+  },
+  components: {
+    Popover: () => import("@/packages/popover")
+  },
+  mounted() {
+    setInterval(()=>{
+      this.title = "hello" + this.count;
+      this.count++;
+      this.date = new Date();
+    }, 2000)
+  },
+  methods: {
+    hello() {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
