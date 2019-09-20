4b0c1bdee0d812d22140e3169f893e3cc2cdd59c
keep2zero <towaywu@gmail.com>
2019-09-20 23:58:36 +0800

    feat: 加入颜色的说明

---
+++ src/app/color.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+     primary: #4d79f6 -> #fff
+     secondary: #4ac7ec -> #fff
+     success: #1ecab8 -> #fff
+     warning: #f3c74d -> #fff
+     info: #9ba7ca -> #fff
+     danger: #f1646c -> #fff
+     dark: #2f4686 -> #fff
+     light: #f1f5fa -> #50649c
+     link: #fff -> #8997bd
+      pink: rgba(255,93,160,.1) -> #ff5da0
+      purple: #7551e9 -> #fff
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
